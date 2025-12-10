d6f60d35cd75b17b8ea906b3c122120712aadc18
ZeeshanLive <41287858+ZeeshanLive@users.noreply.github.com>
2025-12-11 02:01:44 +0530

    Update outputs.tf

--- modules/ecr/outputs.tf
+++ modules/ecr/outputs.tf
@@ -1,3 +1,11 @@
-output "repositories" {
-  value = aws_ecr_repository.this
+# Add to main.tf for debugging
+output "debug_lifecycle_policies" {
+  value = {
+    for repo_name, repo_cfg in local.repos :
+    repo_name => {
+      has_lifecycle_policy = try(repo_cfg.lifecycle_policy, null) != null
+      has_rules = try(length(repo_cfg.lifecycle_policy.rules), 0) > 0
+      rule_count = try(length(repo_cfg.lifecycle_policy.rules), 0)
+    }
+  }
 }
