22815375e5846541daf6cea68a52cf5ab5c03b56
ZeeshanLive <41287858+ZeeshanLive@users.noreply.github.com>
2025-12-11 01:02:03 +0530

    Update variables.tf

--- modules/ecr/variables.tf
+++ modules/ecr/variables.tf
@@ -1,4 +1,36 @@
 variable "repositories" {
-  description = "Map of ECR repositories and their configurations"
-  type        = map(any)
+  description = "Map of ECR repositories to create"
+  type = map(object({
+    image_tag_mutability = optional(string)
+
+    encryption_configuration = object({
+      encryptionType = string
+      kmsKeyId       = optional(string)
+    })
+
+    image_scanning_configuration = object({
+      scanOnPush = bool
+    })
+
+    lifecycle_policy = optional(object({
+      rules = list(object({
+        rulePriority = number
+        description  = optional(string)
+
+        selection = object({
+          tagStatus       = string
+          tagPatternList  = optional(list(string))
+          tagPrefixList   = optional(list(string))
+          countType       = string
+          countUnit       = optional(string)
+          countNumber     = optional(number)
+        })
+
+        action = object({
+          type               = string
+          targetStorageClass = optional(string)
+        })
+      }))
+    }))
+  }))
 }
